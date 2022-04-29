3e8ee13df92930bb4ce4e16f61883da495d4bdfc
Jorge Machuca <jmachuca06@gmail.com>
2022-04-29 07:50:40 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_course/screens/listview2_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Listview2Screen(),
+    );
+  }
+}
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const [
+    "Megaman asdfads",
+    "Metal Gear Solid  ",
+    "Super Smash Bros.",
+    "WOW",
+    "Cachaca"
+  ];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Tipo 2 '),
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        separatorBuilder: (_, __) => const Divider(),
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ),
+      ),
+    );
+  }
+}
